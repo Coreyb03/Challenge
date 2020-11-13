2b73a1abc57f21c18b834283c9808c74624b888b
Corey Becker <beckercn1@s.dcsdk12.org>
2020-11-13 14:23:19 -0700

    creates a lunch account with a unique id and allows that account to add money and buy lunch

--- LunchAccount.java
+++ LunchAccount.java
@@ -1,31 +1,61 @@
 public class LunchAccount{
-    private String name;
     private String id;
     private double balance;
+    private double totalMoneySpent;
+    private int totalLunchs;
+    private int timesMoneyAdded;
 
-    public LunchAccount(String name, String id, double balance){
-        this.name = name;
-        this.id = id;
+    private static int studentCount = 0;
+
+    public LunchAccount(double balance){
+        this.studentCount += 1;
+        this.id = Integer.toString(studentCount);
         this.balance = balance;
+        if (studentCount < 100){
+            this.balance += 20.0;
+            this.timesMoneyAdded += 1;
+        }
     }
 
-    public LunchAccount(String name, String id){
-        this.name = name;
-        this.id = id;
+    public LunchAccount(){
         this.balance = 0.0;
+        this.studentCount += 1;
+        this.id = Integer.toString(studentCount);
     }
 
     public double getBalance(){
         return this.balance;
     }
 
+    public String getId(){
+        while ((this.id).length() < 4){
+            this.id = "0" + this.id;
+        }
+        return this.id;
+        
+    }
+
     public void addBalance(double money){
-        this.balance =+ money;
+        this.balance = this.balance + money;
+        if (studentCount < 100 && timesMoneyAdded == 0){
+            this.balance += 20.0;
+        }
+        this.timesMoneyAdded += 1;
+    }
+
+    public int getTotalLunchs(){
+        return this.totalLunchs;
+    }
+
+    public double getTotalMoneySpent(){
+        return this.totalMoneySpent;
     }
 
     public String buyFood(double price){
         if (this.balance >= price){
             this.balance -= price;
+            this.totalLunchs += 1;
+            this.totalMoneySpent += price;
             return "Remaining balance: " + Double.toString(balance);
         } else {
             return "Transaction Denied";
--- LunchTester.java
+++ LunchTester.java
@@ -1,16 +1,30 @@
 public class LunchTester{
     public static void main(String[] args) {
-        LunchAccount randy = new LunchAccount("Randy", "1234");
+        LunchAccount randy = new LunchAccount();
         
         System.out.println(randy.getBalance());
-        randy.addBalance(20.57);
-        System.out.println(randy.getBalance());
+        randy.addBalance(19.57);
+        System.out.println(randy.getBalance()); //39.57
         
         System.out.println(randy.buyFood(9.99));
-        System.out.println(randy.buyFood(99.99));
+        System.out.println(randy.buyFood(99.99)); //denied
 
+        System.out.println(randy.getBalance());
         randy.addBalance(90.42);
         System.out.println(randy.getBalance());
+
+        System.out.println(randy.getId());
+
+        LunchAccount sandy = new LunchAccount(70.00);
+        System.out.println(sandy.getId());
+
+        System.out.println(randy.buyFood(21.50));
+        System.out.println(randy.buyFood(3.50));
+        System.out.println(randy.getTotalLunchs()); //3
+        System.out.println(randy.getTotalMoneySpent()); //34.99
+
+        sandy.addBalance(10.00);
+        System.out.println(sandy.getBalance());//100.00
 
     }
 }
