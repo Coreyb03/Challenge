e142785486d525f696b65126098d611c027ea7b4
Corey Becker <beckercn1@s.dcsdk12.org>
2021-01-11 11:35:01 -0700

    sorts arrays 3 diffrent ways

--- ArrayChallenge.java
+++ ArrayChallenge.java
@@ -2,8 +2,16 @@ public class ArrayChallenge{
     public static void main(String[] args) {
         int[] data = {2,6,40,10,5};
         System.out.println(search(data, 10));
-        sort(data);
+        bubbleSort(data);
         printArray(data);
+
+        int[] data2 = {3,55,7,1,999};
+        insertionSort(data2);
+        printArray(data2);
+
+        int[] data3 = {1,4,88,25,9};
+        selectionSort(data3);
+        printArray(data3);
     }
 
     public static int search(int[] data, int number){
@@ -15,7 +23,7 @@ public class ArrayChallenge{
         return -1;
     }
 
-    public static void sort(int[] data){
+    public static void bubbleSort(int[] data){
         for (int x = 1; x < data.length; x++ ){
             for (int i =0; i < data.length - x; i++){
                 if (data[i] > data[i+1]){
@@ -26,6 +34,33 @@ public class ArrayChallenge{
             }
         }
     }
+
+    public static void insertionSort(int[] data){ 
+        for (int i = 1; i < data.length; ++i) { 
+            int temp = data[i]; 
+            int j = i - 1; 
+            while (j >= 0 && data[j] > temp) { 
+                data[j + 1] = data[j]; 
+                j = j - 1; 
+            } 
+            data[j + 1] = temp; 
+        } 
+    }
+
+    public static void selectionSort(int[] data){
+        for(int i = 0; i < data.length-1; i++){ 
+            int min = i;
+            for(int j = i+1; j < data.length; j++){
+                if (data[min] > data[j]){
+                    min = j;
+                }
+            }
+            int temp = data[min]; 
+            data[min] = data[i]; 
+            data[i] = temp;
+        }
+    }
+
     public static void printArray(int[] data){
         for (int i =0; i < data.length; i++){
             System.out.println(data[i]);
